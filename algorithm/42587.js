4350d39c26d519a5a8424aecf4f3c3f96acb3e99
youjung <dbwjdrkddls1@gmail.com>
2023-03-15 23:56:44 +0900

    [알고리즘]추가

--- algorithm/42587.js
+++ algorithm/42587.js
@@ -1,24 +1,21 @@
-//반례를 찾지 못하였다. 찾는대로 수정 할 예정
 function solution(priorities, location) {
   let bigNum = priorities.indexOf(Math.max(...priorities));
   let arr = [];
-  let hi = bigNum;
 
   for (let i = 0; i < priorities.length; i++) {
-    if (i >= priorities.length) {
-      hi = 0;
-    }
-
-    let subNum = priorities.indexOf(Math.max(...priorities), hi);
+    let subNum = priorities.indexOf(Math.max(...priorities), bigNum);
 
     if (subNum === -1) {
       subNum = priorities.indexOf(Math.max(...priorities));
-      hi = subNum;
+      bigNum = subNum;
+    } else {
+      bigNum = subNum;
     }
 
     priorities[subNum] = 0;
     arr.push(subNum);
-    hi++;
+    bigNum++;
   }
+
   return arr.indexOf(location) + 1;
 }
