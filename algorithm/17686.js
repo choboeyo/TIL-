5d28559855fe5f3bcfd80d42293a9647e68c9219
youjung <dbwjdrkddls1@gmail.com>
2023-05-28 19:37:15 +0900

    [ㅇㅏㄹ고리즘]

---
+++ algorithm/17686.js
@@ -0,0 +1,18 @@
+function solution(files) {
+  const re = /^([a-zA-Z-\. ]+)([0-9]+)(.*)$/;
+  let dict = [];
+  files.forEach((entry, idx) => {
+    let [fn, head, num] = entry.match(re);
+    dict.push({ fn, head: head.toLowerCase(), num: parseInt(num), idx });
+  });
+
+  return dict
+    .sort((a, b) => {
+      if (a.head > b.head) return 1;
+      if (a.head < b.head) return -1;
+      if (a.num > b.num) return 1;
+      if (a.num < b.num) return -1;
+      return a - b;
+    })
+    .map((el) => el.fn);
+}
